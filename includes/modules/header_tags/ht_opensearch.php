b0c46bd8704a4dda3fa8cafd03d295ea2bcef26e
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-12-30 14:20:20 -0500

    small update

--- includes/modules/header_tags/ht_opensearch.php
+++ includes/modules/header_tags/ht_opensearch.php
@@ -60,10 +60,10 @@
 
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Souhaitez vous activer ce module ?',
+          'configuration_title' => 'Do you want to install this module ?',
           'configuration_key' => 'MODULE_HEADER_TAGS_OPENSEARCH_STATUS',
           'configuration_value' => 'True',
-          'configuration_description' => 'Souhaitez vous activer ce module ?',
+          'configuration_description' => 'Do you want to install this module ?',
           'configuration_group_id' => '6',
           'sort_order' => '1',
           'set_function' => 'clic_cfg_set_boolean_value(array(\'True\', \'False\'))',
@@ -169,10 +169,10 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Ordre de tri d\'affichage',
+          'configuration_title' => 'Sort Order',
           'configuration_key' => 'MODULE_HEADER_TAGS_OPENSEARCH_SORT_ORDER',
           'configuration_value' => '150',
-          'configuration_description' => 'Ordre de tri pour l\'affichage (Le plus petit nombre est montré en premier)',
+          'configuration_description' => 'Sort order. Lowest is displayed in first',
           'configuration_group_id' => '6',
           'sort_order' => '130',
           'set_function' => '',
